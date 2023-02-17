12da4d12c2bf2471a474448bcbaa13f4f6e3358c
Kan-A-Pesh <michelm.gaming@gmail.com>
2023-02-17 13:06:44 +0100

    ✨ Added `notification` and `post` controllers

--- api/controllers/notification.php
+++ api/controllers/notification.php
@@ -3,6 +3,7 @@
 class Notification
 {
     private string $id;
+    public string $recipient_handle;
     public string $icon_path;
     public string $content;
     public DateTime $created_at;
@@ -15,5 +16,92 @@ class Notification
     public function getId(): string
     {
         return $this->id;
+    }
+
+    /**
+     * Create a new notification
+     * 
+     * @param string $recipient_handle The recipient handle
+     * @param string $icon_path The path to the icon
+     * @param string $content The content of the notification
+     * @return Notification|int The created notification or an error code
+     */
+    public static function create(string $recipient_handle, string $icon_path, string $content): Notification|int
+    {
+        global $MYSQL_NOTIFICATION_TABLE;
+
+        // Get the notification ID
+        $notification_id = generate_uuid();
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "INSERT INTO $MYSQL_NOTIFICATION_TABLE
+                (PK_notification_id, FK_recipient_handle, icon_path, content)
+                VALUES (:notification_id, :recipient_handle, :icon_path, :content)"
+            );
+
+            $stmt->bindParam(":notification_id", $notification_id);
+            $stmt->bindParam(":recipient_handle", $recipient_handle);
+            $stmt->bindParam(":icon_path", $icon_path);
+            $stmt->bindParam(":content", $content);
+            $stmt->execute();
+
+            $notification = new Notification($notification_id);
+            $notification->recipient_handle = $recipient_handle;
+            $notification->icon_path = $icon_path;
+            $notification->content = $content;
+            $notification->created_at = new DateTime();
+
+            return $notification;
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
+    /**
+     * Get all notifications for a user and delete them
+     * 
+     * @param string $recipient_handle The recipient handle
+     * @return array|int The notifications or an error code
+     */
+    public static function pop_all(string $recipient_handle): array|int
+    {
+        global $MYSQL_NOTIFICATION_TABLE;
+
+        try {
+            // Get the notifications
+            $stmt = Database::$pdo->prepare(
+                "SELECT * FROM $MYSQL_NOTIFICATION_TABLE
+                WHERE FK_recipient_handle = :recipient_handle
+                ORDER BY created_at DESC"
+            );
+
+            $stmt->bindParam(":recipient_handle", $recipient_handle);
+            $stmt->execute();
+
+            $notifications = [];
+            while ($row = $stmt->fetch()) {
+                $notification = new Notification($row["PK_notification_id"]);
+                $notification->recipient_handle = $row["FK_recipient_handle"];
+                $notification->icon_path = $row["icon_path"];
+                $notification->content = $row["content"];
+                $notification->created_at = new DateTime($row["created_at"]);
+
+                $notifications[] = $notification;
+            }
+
+            // Delete the notifications
+            $stmt = Database::$pdo->prepare(
+                "DELETE FROM $MYSQL_NOTIFICATION_TABLE
+                WHERE FK_recipient_handle = :recipient_handle"
+            );
+
+            $stmt->bindParam(":recipient_handle", $recipient_handle);
+            $stmt->execute();
+
+            return $notifications;
+        } catch (PDOException $e) {
+            return 500;
+        }
     }
 }
--- api/controllers/post.php
+++ api/controllers/post.php
@@ -15,11 +15,305 @@ class Post
 
     public function __construct(string $id)
     {
+        global $MYSQL_POST_TABLE, $MYSQL_LIKE_TABLE;
+
         $this->id = $id;
+        
+        // Get the likes
+        $stmt = Database::$pdo->prepare(
+            "SELECT COUNT(*) FROM $MYSQL_LIKE_TABLE
+            WHERE PFK_post_id = :id"
+        );
+
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+
+        $this->likes = $stmt->fetchColumn();
+
+        // Get the comments
+        $stmt = Database::$pdo->prepare(
+            "SELECT COUNT(*) FROM $MYSQL_POST_TABLE
+            WHERE FK_reply_to = :id"
+        );
+
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        
+        $this->comments = $stmt->fetchColumn();
     }
 
     public function getId(): string
     {
         return $this->id;
     }
+
+    /**
+     * Convert the media paths string to an array of paths
+     *
+     * @param string $media_list_paths The media paths string
+     * @return array The media paths array
+     */
+    private static function get_media_paths(string $media_list_paths): array
+    {
+        $media_list_count = strlen($media_list_paths) / 20;
+        
+        $media_paths = [];
+        for ($i = 0; $i < $media_list_count; $i++) {
+            $media_paths[] = substr($media_list_paths, $i * 20, 20);
+        }
+
+        return $media_paths;
+    }
+
+    /**
+     * Get a post by its ID
+     * 
+     * @param string $id The post ID
+     * @return Post|int The post or an error code
+     */
+    public static function get(string $id): Post|int
+    {
+        global $MYSQL_POST_TABLE;
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "SELECT * FROM $MYSQL_POST_TABLE
+                WHERE PK_post_id = :id"
+            );
+
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+
+            $row = $stmt->fetch();
+
+            $post = new Post($row["PK_post_id"]);
+            $post->reply_to = $row["FK_reply_to"];
+            $post->author_handle = $row["FK_author_handle"];
+            $post->tag = $row["tag"];
+            $post->content = $row["content"];
+            $post->views = $row["views_count"];
+            $post->created_at = new DateTime($row["created_at"]);
+            $post->media_paths = self::get_media_paths($row["media_list_paths"]);
+
+            return $post;
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
+    /**
+     * Get all posts that match the given criteria
+     *
+     * @param string|null $query The search query (null for no query)
+     * @param array|null $fromUsers The users to get posts from (null for all users)
+     * @param array|null $excludeUsers The users to exclude posts from (null for no users)
+     * @param string|null $replyTo The post ID to get replies to (null for no replies)
+     * @param boolean|null $hasMedia Whether to get posts with/without media (null for all posts)
+     * @param integer $limit The maximum number of posts to get
+     * @param integer $offset The offset of the posts to get
+     * @return array The posts
+     */
+    public static function get_all(
+        string $query = null,
+        array $fromUsers = null,
+        array $excludeUsers = null,
+        string $replyTo = null,
+        bool $hasMedia = null,
+        int $limit = 25,
+        int $offset = 0
+    ): array
+    {
+        global $MYSQL_POST_TABLE;
+
+        $query = $query ? "%$query%" : null;
+        $fromUsers = $fromUsers ? implode("', '", $fromUsers) : null;
+        $excludeUsers = $excludeUsers ? implode("', '", $excludeUsers) : null;
+        $hasMedia = $hasMedia !== null ? ($hasMedia ? "NOT" : "") : null;
+
+        $stmt = Database::$pdo->prepare(
+            "SELECT * FROM $MYSQL_POST_TABLE
+            WHERE (:query IS NULL OR content LIKE :query)
+            AND (:fromUsers IS NULL OR FK_author_handle IN ('$fromUsers'))
+            AND (:excludeUsers IS NULL OR FK_author_handle NOT IN ('$excludeUsers'))
+            AND (:replyTo IS NULL OR FK_reply_to = :replyTo)
+            AND (:hasMedia IS NULL OR media_list_paths $hasMedia LIKE '')
+            ORDER BY created_at DESC
+            LIMIT :limit OFFSET :offset"
+        );
+
+        $stmt->bindParam(":query", $query);
+        $stmt->bindParam(":fromUsers", $fromUsers);
+        $stmt->bindParam(":excludeUsers", $excludeUsers);
+        $stmt->bindParam(":replyTo", $replyTo);
+        $stmt->bindParam(":hasMedia", $hasMedia);
+        $stmt->bindParam(":limit", $limit, PDO::PARAM_INT);
+        $stmt->bindParam(":offset", $offset, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $posts = [];
+        while ($row = $stmt->fetch()) {
+            $post = new Post($row["PK_post_id"]);
+            $post->reply_to = $row["FK_reply_to"];
+            $post->author_handle = $row["FK_author_handle"];
+            $post->tag = $row["tag"];
+            $post->content = $row["content"];
+            $post->views = $row["views_count"];
+            $post->created_at = new DateTime($row["created_at"]);
+            $post->media_paths = self::get_media_paths($row["media_list_paths"]);
+
+            $posts[] = $post;
+        }
+
+        return $posts;
+    }
+
+    /**
+     * Create a new post and return it
+     *
+     * @param string $author_handle The author's handle
+     * @param integer $tag The post tag
+     * @param string $content The post content
+     * @param array $media_paths The media paths
+     * @param string|null $reply_to The post ID to reply to (null for no reply)
+     * @return Post|integer The post or an error code
+     */
+    public static function create(
+        string $author_handle,
+        int $tag,
+        string $content,
+        array $media_paths,
+        string $reply_to = null
+    ): Post|int
+    {
+        global $MYSQL_POST_TABLE;
+
+        $media_list_paths = implode("", $media_paths);
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "INSERT INTO $MYSQL_POST_TABLE
+                (FK_author_handle, tag, content, media_list_paths, FK_reply_to)
+                VALUES (:author_handle, :tag, :content, :media_list_paths, :reply_to)"
+            );
+
+            $stmt->bindParam(":author_handle", $author_handle);
+            $stmt->bindParam(":tag", $tag);
+            $stmt->bindParam(":content", $content);
+            $stmt->bindParam(":media_list_paths", $media_list_paths);
+            $stmt->bindParam(":reply_to", $reply_to);
+            $stmt->execute();
+
+            return self::get(Database::$pdo->lastInsertId());
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
+    /**
+     * Update a post and return it
+     *
+     * @param string $id The post ID
+     * @return null|integer Null or an error code
+     */
+    public static function delete(string $id): null|int
+    {
+        global $MYSQL_POST_TABLE;
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "DELETE FROM $MYSQL_POST_TABLE
+                WHERE PK_post_id = :id"
+            );
+
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+
+            return null;
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
+    /**
+     * Like a post
+     * 
+     * @param string $post_id The post ID
+     * @param string $user_handle The user handle
+     * @return null|int Null or an error code
+     */
+    public static function like_post(string $post_id, string $user_handle): null|int
+    {
+        global $MYSQL_LIKE_TABLE;
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "INSERT INTO $MYSQL_LIKE_TABLE
+                (PFK_post_id, PFK_user_handle)
+                VALUES (:post_id, :user_handle)"
+            );
+
+            $stmt->bindParam(":post_id", $post_id);
+            $stmt->bindParam(":user_handle", $user_handle);
+            $stmt->execute();
+
+            return null;
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
+    /**
+     * Unlike a post
+     *
+     * @param string $post_id The post ID
+     * @param string $user_handle The user handle
+     * @return null|integer Null or an error code
+     */
+    public static function unlike_post(string $post_id, string $user_handle): null|int
+    {
+        global $MYSQL_LIKE_TABLE;
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "DELETE FROM $MYSQL_LIKE_TABLE
+                WHERE PFK_post_id = :post_id AND PFK_user_handle = :user_handle"
+            );
+
+            $stmt->bindParam(":post_id", $post_id);
+            $stmt->bindParam(":user_handle", $user_handle);
+            $stmt->execute();
+
+            return null;
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
+    /**
+     * Check if a post is liked by a user
+     *
+     * @param string $post_id The post ID
+     * @param string $user_handle The user handle
+     * @return bool|int True if liked, false if not, or an error code
+     */
+    public static function is_liked(string $post_id, string $user_handle): bool
+    {
+        global $MYSQL_LIKE_TABLE;
+
+        try {
+            $stmt = Database::$pdo->prepare(
+                "SELECT * FROM $MYSQL_LIKE_TABLE
+                WHERE PFK_post_id = :post_id AND PFK_user_handle = :user_handle"
+            );
+
+            $stmt->bindParam(":post_id", $post_id);
+            $stmt->bindParam(":user_handle", $user_handle);
+            $stmt->execute();
+
+            return $stmt->rowCount() > 0;
+        } catch (PDOException $e) {
+            return 500;
+        }
+    }
+
 }
