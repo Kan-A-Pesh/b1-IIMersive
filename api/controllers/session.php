fb414df4a3ab3ab409d8493b58634210be0333bb
Kan-A-Pesh <michelm.gaming@gmail.com>
2023-02-17 09:06:07 +0100

    ✨ Added `session` controller

--- api/controllers/session.php
+++ api/controllers/session.php
@@ -45,6 +45,7 @@ class Session
         // Create session id and expiration date
         $session_id = generate_uuid();
         $expires_at = new DateTime("+15 minutes");
+        $expires_at_string = $expires_at->format("Y-m-d H:i:s");
 
         // Create session
         try {
@@ -55,7 +56,7 @@ class Session
 
             $stmt->bindParam(":session_id", $session_id);
             $stmt->bindParam(":user_handle", $user_handle);
-            $stmt->bindParam(":expires_at", $expires_at->format("Y-m-d H:i:s"));
+            $stmt->bindParam(":expires_at", $expires_at_string);
             $stmt->bindParam(":ip", $ip);
             $stmt->bindParam(":user_agent", $user_agent);
 
@@ -90,8 +91,17 @@ class Session
 
         // Create session expiration date
         $expires_at = new DateTime("+15 minutes");
+        $expires_at_string = $expires_at->format("Y-m-d H:i:s");
 
-        // 
+        // Check if session exists and is valid
+        $session = self::get($session_id);
+        if ($session === null) {
+            return 404;
+        } else if (!$session->isValid()) {
+            return 410;
+        } else if ($session->ip !== $ip || $session->user_agent !== $user_agent) {
+            return 401;
+        }
 
         // Update session
         try {
@@ -102,17 +112,89 @@ class Session
             );
 
             $stmt->bindParam(":session_id", $session_id);
-            $stmt->bindParam(":expires_at", $expires_at->format("Y-m-d H:i:s"));
+            $stmt->bindParam(":expires_at", $expires_at_string);
             $stmt->bindParam(":ip", $ip);
             $stmt->bindParam(":user_agent", $user_agent);
 
             $stmt->execute();
 
             // Return
-            return;
+            return null;
         } catch (PDOException $e) {
             // Return error code
             return 500;
         }
     }
+
+    /**
+     * Get a session
+     * 
+     * @param string $session_id - The session's id
+     * @return Session|int - The session or an error code
+     */
+    public static function get(string $session_id): Session|int
+    {
+        global $MYSQL_SESSION_TABLE;
+
+        // Get session
+        try {
+            $stmt = Database::$pdo->prepare(
+                "SELECT * FROM $MYSQL_SESSION_TABLE
+                WHERE PK_session_id = :session_id"
+            );
+
+            $stmt->bindParam(":session_id", $session_id);
+
+            $stmt->execute();
+
+            // Check if session exists
+            if ($stmt->rowCount() === 0) {
+                return 404;
+            }
+
+            // Return session
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $session = new Session($result["PK_session_id"]);
+            $session->user_handle = $result["FK_user_handle"];
+            $session->created_at = new DateTime($result["created_at"]);
+            $session->expires_at = new DateTime($result["expires_at"]);
+            $session->ip = $result["ip"];
+            $session->user_agent = $result["user_agent"];
+
+            return $session;
+        } catch (PDOException $e) {
+            // Return error code
+            return 500;
+        }
+    }
+
+    /**
+     * Delete a session
+     * 
+     * @param string $session_id - The session's id
+     * @return null|int - Null if successful, an error code otherwise
+     */
+    public static function delete(string $session_id): null|int
+    {
+        global $MYSQL_SESSION_TABLE;
+
+        // Delete session
+        try {
+            $stmt = Database::$pdo->prepare(
+                "DELETE FROM $MYSQL_SESSION_TABLE
+                WHERE PK_session_id = :session_id"
+            );
+
+            $stmt->bindParam(":session_id", $session_id);
+
+            $stmt->execute();
+
+            // Return
+            return null;
+        } catch (PDOException $e) {
+            // Return error code
+            return 500;
+        }
+    }
+
 }
