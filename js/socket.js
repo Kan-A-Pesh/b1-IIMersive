f9a720318ca21b8f7da8978e17bad80698e44bf8
Kan-A-Pesh <michelm.gaming@gmail.com>
2023-04-16 22:59:52 +0200

    🐛 Fixed null exception (again)

--- js/socket.js
+++ js/socket.js
@@ -1,8 +1,9 @@
+let ADDRESS = undefined;
 if (window.location.host.includes("localhost")) {
     ADDRESS = "ws://localhost:5556";
 }
 
-const socket = io(ADDRESS || undefined);
+const socket = io(ADDRESS);
 
 socket.on('connect', () => {
     console.log("SYSM: Connected to socket server");
